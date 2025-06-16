16f603671e18f2567a8188db9282e12175969e3f
Inseihou <vinyowlz@gmail.com>
2025-06-16 20:13:07 +0700

    uas2

--- script.js
+++ script.js
@@ -1,267 +1,320 @@
-var JoyStick = (function(container, parameters, callback)
-{
-    parameters = parameters || {};
-    var title = (typeof parameters.title === "undefined" ? "joystick" : parameters.title),
-        width = (typeof parameters.width === "undefined" ? 0 : parameters.width),
-        height = (typeof parameters.height === "undefined" ? 0 : parameters.height),
-        internalFillColor = (typeof parameters.internalFillColor === "undefined" ? "#00AA00" : parameters.internalFillColor),
-        internalLineWidth = (typeof parameters.internalLineWidth === "undefined" ? 2 : parameters.internalLineWidth),
-        internalStrokeColor = (typeof parameters.internalStrokeColor === "undefined" ? "#003300" : parameters.internalStrokeColor),
-        externalLineWidth = (typeof parameters.externalLineWidth === "undefined" ? 2 : parameters.externalLineWidth),
-        externalStrokeColor = (typeof parameters.externalStrokeColor ===  "undefined" ? "#008000" : parameters.externalStrokeColor),
-        autoReturnToCenter = (typeof parameters.autoReturnToCenter === "undefined" ? true : parameters.autoReturnToCenter);
-
-    callback = callback || function(StickStatus) {};
-
-var objContainer = document.getElementById(container);
-objContainer.style.touchAction = "none";
-
-    var canvas = document.createElement("canvas");
-    canvas.id = title;
-    if(width === 0) { width = objContainer.clientWidth; }
-    if(height === 0) { height = objContainer.clientHeight; }
-    canvas.width = width;
-    canvas.height = height;
-    objContainer.appendChild(canvas);
-    var context=canvas.getContext("2d");
-
-    var pressed = 0; // Bool - 1=Yes - 0=No
-    var circumference = 2 * Math.PI;
-    var internalRadius = (canvas.width-((canvas.width/2)+10))/2;
-    var maxMoveStick = internalRadius + 5;
-    var externalRadius = internalRadius + 30;
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var directionHorizontalLimitPos = canvas.width / 10;
-    var directionHorizontalLimitNeg = directionHorizontalLimitPos * -1;
-    var directionVerticalLimitPos = canvas.height / 10;
-    var directionVerticalLimitNeg = directionVerticalLimitPos * -1;
-    var movedX=centerX;
-    var movedY=centerY;
-    if("ontouchstart" in document.documentElement)
-        {
-            canvas.addEventListener("touchstart", onTouchStart, false);
-            document.addEventListener("touchmove", onTouchMove, false);
-            document.addEventListener("touchend", onTouchEnd, false);
-        }
-        else
-        {
-            canvas.addEventListener("mousedown", onMouseDown, false);
-            document.addEventListener("mousemove", onMouseMove, false);
-            document.addEventListener("mouseup", onMouseUp, false);
-        }
-        drawExternal();
-        drawInternal();
-        function drawExternal()
-        {
-            context.beginPath();
-            context.arc(centerX, centerY, externalRadius, 0, circumference, false);
-            context.lineWidth = externalLineWidth;
-            context.strokeStyle = externalStrokeColor;
-            context.stroke();
-        }
-
-        function drawInternal()
-    {
-        context.beginPath();
-        if(movedX<internalRadius) { movedX=maxMoveStick; }
-        if((movedX+internalRadius) > canvas.width) { movedX = canvas.width-(maxMoveStick); }
-        if(movedY<internalRadius) { movedY=maxMoveStick; }
-        if((movedY+internalRadius) > canvas.height) { movedY = canvas.height-(maxMoveStick); }
-        context.arc(movedX, movedY, internalRadius, 0, circumference, false);
-        // create radial gradient
-        var grd = context.createRadialGradient(centerX, centerY, 5, centerX, centerY, 200);
-        // Light color
-        grd.addColorStop(0, internalFillColor);
-        // Dark color
-        grd.addColorStop(1, internalStrokeColor);
-        context.fillStyle = grd;
-        context.fill();
-        context.lineWidth = internalLineWidth;
-        context.strokeStyle = internalStrokeColor;
-        context.stroke();
-    }
-    let touchId = null;
-    function onTouchStart(event)
-    {
-        pressed = 1;
-        touchId = event.targetTouches[0].identifier;
-    }
-
-    function onTouchMove(event)
-    {
-        if(pressed === 1 && event.targetTouches[0].target === canvas)
-        {
-            movedX = event.targetTouches[0].pageX;
-            movedY = event.targetTouches[0].pageY;
-            // Manage offset
-            if(canvas.offsetParent.tagName.toUpperCase() === "BODY")
-            {
-                movedX -= canvas.offsetLeft;
-                movedY -= canvas.offsetTop;
-            }
-            else
-            {
-                movedX -= canvas.offsetParent.offsetLeft;
-                movedY -= canvas.offsetParent.offsetTop;
-            }
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            // Redraw object
-            drawExternal();
-            drawInternal();
-
-            // Set attribute of callback
-            StickStatus.xPosition = movedX;
-            StickStatus.yPosition = movedY;
-            StickStatus.x = (100*((movedX - centerX)/maxMoveStick)).toFixed();
-            StickStatus.y = ((100*((movedY - centerY)/maxMoveStick))*-1).toFixed();
-            StickStatus.cardinalDirection = getCardinalDirection();
-            callback(StickStatus);
-        }
-    }
-
-    function onTouchEnd(event)
-    {
-        if (event.changedTouches[0].identifier !== touchId) return;
-
-        pressed = 0;
-        if(autoReturnToCenter)
-            {
-                movedX = centerX;
-                movedY = centerY;
-            }
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            drawExternal();
-            drawInternal(); 
-            StickStatus.xPosition = movedX;
-            StickStatus.yPosition = movedY;
-            StickStatus.x = (100*((movedX - centerX)/maxMoveStick)).toFixed();
-            StickStatus.y = ((100*((movedY - centerY)/maxMoveStick))*-1).toFixed();
-            StickStatus.cardinalDirection = getCardinalDirection();
-            callback(StickStatus);
-        }   
-        function onMouseDown(event) 
-    {
-        pressed = 1;
-    }
-    function onMouseMove(event) 
-    {
-        if(pressed === 1)
-        {
-            movedX = event.pageX;
-            movedY = event.pageY;
-            // Manage offset
-            if(canvas.offsetParent.tagName.toUpperCase() === "BODY")
-            {
-                movedX -= canvas.offsetLeft;
-                movedY -= canvas.offsetTop;
-            }
-            else
-            {
-                movedX -= canvas.offsetParent.offsetLeft;
-                movedY -= canvas.offsetParent.offsetTop;
-            }
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            drawExternal();
-            drawInternal();
-            StickStatus.xPosition = movedX;
-            StickStatus.yPosition = movedY;
-            StickStatus.x = (100*((movedX - centerX)/maxMoveStick)).toFixed();
-            StickStatus.y = ((100*((movedY - centerY)/maxMoveStick))*-1).toFixed();
-            StickStatus.cardinalDirection = getCardinalDirection();
-            callback(StickStatus);
-        }
-    }   
-    function onMouseUp(event) 
-    {
-        pressed = 0;
-        // If required reset position store variable
-        if(autoReturnToCenter)
-        {
-            movedX = centerX;
-            movedY = centerY;
-        }
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        drawExternal();
-        drawInternal();
-        StickStatus.xPosition = movedX;
-        StickStatus.yPosition = movedY;
-        StickStatus.x = (100*((movedX - centerX)/maxMoveStick)).toFixed();
-        StickStatus.y = ((100*((movedY - centerY)/maxMoveStick))*-1).toFixed();
-        StickStatus.cardinalDirection = getCardinalDirection();
-        callback(StickStatus);
-    }
-
-    function getCardinalDirection()
-    {
-        let result = "";
-        let orizontal = movedX - centerX;
-        let vertical = movedY - centerY;
+let Joycon = {
+  
+    // event listeners
+    listeners: {},
+    
+    // controllers
+    controller: {},
+    
+    // controller utility functions
+    controllers: {
+      
+      on: {
         
-        if(vertical >= directionVerticalLimitNeg && vertical <= directionVerticalLimitPos)
-        {
-            result = "C";
-        }
-        if(vertical < directionVerticalLimitNeg)
-        {
-            result = "N";
-        }
-        if(vertical > directionVerticalLimitPos)
-        {
-            result = "S";
+        // a, b, x, y, left-shoulder, right-shoulder, left-trigger, right-trigger, select, start, left-joystick, right-joystick, dpad-up, dpad-down, dpad-left, dpad-right, home, share
+        press: (key, callback) => {
+          
+          Joycon.listeners[key] = {
+            callback: callback,
+            lastValue: 0
+          };
+          
+        },
+        
+        // left-joystick, right-joystick
+        move: (key, callback) => {
+          
+          Joycon.listeners[key + '-move'] = {
+            callback: callback,
+            lastValue: {
+              x: 0,
+              y: 0
+            }
+          };
+          
+        },
+        
+        
+        connect: (callback) => {
+          
+          Joycon.listeners['controller-connect'] = {
+            callback: callback
+          };
+                  
+        },
+        
+        disconnect: (callback) => {
+          
+          Joycon.listeners['controller-disconnect'] = {
+            callback: callback
+          };
+                  
         }
         
-        if(orizontal < directionHorizontalLimitNeg)
-        {
-            if(result === "C")
-            { 
-                result = "W";
-            }
-            else
-            {
-                result += "W";
-            }
-        }
-        if(orizontal > directionHorizontalLimitPos)
-        {
-            if(result === "C")
-            { 
-                result = "E";
-            }
-            else
-            {
-                result += "E";
-            }
+      },
+      
+      // remove the listener for move events
+      // by adding '-move' to the event name, eg.
+      // left-joystick-move
+      removeListener: (name) => {
+        
+        delete Joycon.listeners[name];
+        
+      },
+      
+      // intensity:
+      // { preset } or { mildMotorIntensity, strongMotorIntensity }
+      // presets:
+      // 'mild', 'medium', 'strong'
+      vibrate: async (intensity, duration) => {
+        
+        // if chosen a preset
+        if (intensity.preset) {
+          
+          // load preset
+          const presets = Joycon.controllers.vibrationPresets;
+          const preset = presets[intensity.preset];
+          
+          intensity = preset;
+          
         }
         
-        return result;
+        // parse vibration options
+        const vibrationOptions = {
+          duration: duration,
+          strongMagnitude: intensity.strongMotorIntensity,
+          weakMagnitude: intensity.mildMotorIntensity
+        };
+        
+        
+        // vibrate all controllers
+        
+        const controllers = Object.values(Joycon.controller);
+        
+        controllers.forEach(controller => {
+                
+          const vibrationMotor = controller.vibrationActuator;
+          
+          if (vibrationMotor) {
+            
+            // vibrate
+            vibrationMotor.playEffect('dual-rumble', vibrationOptions);
+            
+          }
+          
+        });
+        
+        // await vibration end
+        await new Promise((resolve) => {
+          window.setTimeout(resolve, duration);
+        });
+        
+      },
+      
+      vibrationPresets: {
+  
+        mild: {
+          strongMotorIntensity: 0,
+          mildMotorIntensity: 0.07
+        },
+  
+        medium: {
+          strongMotorIntensity: 0,
+          mildMotorIntensity: 0.14
+        },
+  
+        strong: {
+          strongMotorIntensity: 1,
+          mildMotorIntensity: 1
+        }
+  
+      }
+      
+    },
+    
+    update: () => {
+      
+      Joycon.updateControllers();
+      
+      
+      const buttonMap = Joycon.buttonMap;
+      const axisMap = Joycon.axisMap;
+      
+      const controllers = Object.values(Joycon.controller);
+      
+      controllers.forEach(controller => {
+        
+        const buttons = controller.buttons;
+        
+        buttons.forEach((button, index) => {
+          
+          const buttonName = buttonMap[index];
+          
+          const buttonListener = Joycon.listeners[buttonName];
+          
+          // if button listener exists
+          if (buttonListener) {
+            
+            // if the button's value changed
+            if (button.value !== buttonListener.lastValue) {
+              
+              buttonListener.lastValue = button.value;
+              
+              // call button listener with button value
+              buttonListener.callback(button.value);
+              
+            }
+  
+          }
+          
+        });
+        
+        
+        const axesObj = controller.axes;
+        let axes = {};
+        
+        // map axes to object
+        axesObj.forEach((axisValue, index) => {
+          
+          const axis = axisMap[index][0];
+          const axisName = axisMap[index][1];
+          
+          if (!axes[axisName]) axes[axisName] = {};
+          axes[axisName][axis] = axisValue;
+          
+        });
+        
+        // run on all axes
+        Object.keys(axes).forEach(axisName => {
+          
+          const axisListener = Joycon.listeners[axisName];
+          
+          // if axis listener exists
+          if (axisListener) {
+            
+            const axisObj = axes[axisName];
+            
+            // if the axis' value changed
+            if (axisObj.x !== axisListener.lastValue.x ||
+                axisObj.y !== axisListener.lastValue.y) {
+              
+              axisListener.lastValue = axisObj;
+              
+              // call axis listener with axis value
+              axisListener.callback({
+                x: axisObj.x,
+                y: axisObj.y
+              });
+              
+            }
+  
+          }
+          
+        });
+        
+      });
+      
+      
+      // if controllers are connected
+      if (Object.keys(Joycon.controller).length
+           !== 0) {
+        
+        // update
+        Joycon.onNextFrame(Joycon.update);
+        
+      }
+      
+    },
+    
+    updateControllers: () => {
+      
+      const controllers = navigator.getGamepads();
+      
+      controllers.forEach(controller => {
+        
+        if (controller && controller.index !== undefined) {
+          
+          Joycon.controller[controller.index] = controller;
+          
+        }
+        
+      });
+      
+    },
+    
+    addListeners: () => {
+      
+      window.addEventListener('gamepadconnected', (e) => {
+        
+        Joycon.controller[e.gamepad.index] = e.gamepad;
+        
+        Joycon.onNextFrame(Joycon.update);
+        
+        
+        const controllerListener = Joycon.listeners['controller-connect'];
+        
+        // if controller listener exists
+        if (controllerListener) {
+          
+          // call controller listener
+          controllerListener.callback(e.gamepad);
+          
+        }
+        
+      });
+      
+      window.addEventListener('gamepaddisconnected', (e) => {
+        
+        delete Joycon.controller[e.gamepad.index];
+        
+        
+        const controllerListener = Joycon.listeners['controller-disconnect'];
+        
+        // if controller listener exists
+        if (controllerListener) {
+          
+          // call controller listener
+          controllerListener.callback(e.gamepad);
+          
+        }
+        
+      });
+      
+    },
+    
+    onNextFrame: (callback) => {
+      window.requestAnimationFrame(callback);
+    },
+    
+    buttonMap: {
+      0: 'a',
+      1: 'b',
+      2: 'x',
+      3: 'y',
+      4: 'left-shoulder',
+      5: 'right-shoulder',
+      6: 'left-trigger',
+      7: 'right-trigger',
+      8: 'select',
+      9: 'start',
+      10: 'left-joystick',
+      11: 'right-joystick',
+      12: 'dpad-up',
+      13: 'dpad-down',
+      14: 'dpad-left',
+      15: 'dpad-right',
+      16: 'home',
+      17: 'share'
+    },
+    
+    axisMap: {
+      0: ['x', 'left-joystick-move'],
+      1: ['y', 'left-joystick-move'],
+      2: ['x', 'right-joystick-move'],
+      3: ['y', 'right-joystick-move'],
     }
-    this.GetWidth = function () 
-    {
-        return canvas.width;
-    };
-    this.GetHeight = function () 
-    {
-        return canvas.height;
-    };  
-    this.GetPosX = function ()
-    {
-        return movedX;
-    };
-    this.GetPosY = function ()
-    {
-        return movedY;
-    };
-    this.GetX = function ()
-    {
-        return (100*((movedX - centerX)/maxMoveStick)).toFixed();
-    };
-    this.GetY = function ()
-    {
-        return ((100*((movedY - centerY)/maxMoveStick))*-1).toFixed();
-    };
-    this.GetDir = function()
-    {
-        return getCardinalDirection();
-    };
-});
+    
+  };
+  
+  Joycon.addListeners();
+
